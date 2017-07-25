70b95d488f16250635fa82283fcf377f1d757fec
changcrazy <changocean@163.com>
2017-07-25 18:48:07 +0800

    123

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>{{ msg }}</div>
+  <div class="index">
+      <mt-swipe :auto="4000" class="ad">
+        <mt-swipe-item><img src="//gw.alicdn.com/tps/TB1s.XCKFXXXXbRXFXXXXXXXXXX-750-176.png"  @click.native="gotoad()"/></mt-swipe-item>
+        <mt-swipe-item><img src="//gw.alicdn.com/tps/TB1s.XCKFXXXXbRXFXXXXXXXXXX-750-176.png"  @click.native="gotoad()"/></mt-swipe-item>
+        <mt-swipe-item><img src="//gw.alicdn.com/tps/TB1s.XCKFXXXXbRXFXXXXXXXXXX-750-176.png"  @click.native="gotoad()"/></mt-swipe-item>
+      </mt-swipe>
+     <div class="newsings">
+
+    </div>
+  </div>
 </template>
 
 
 <script>
+import { Swipe, SwipeItem } from 'mint-ui';
+
 export default {
   name: 'index',
   data () {
     return {
       msg: '123123'
+    }
+  },
+  components: [Swipe, SwipeItem],
+  methods: {
+    gotoad() {
+      this.$router.redirect('/')
     }
   }
 }
@@ -16,5 +33,10 @@ export default {
 
 
 <style>
-
+.ad {
+  height: 100px;
+}
+.ad img {
+  width: 100%;
+}
 </style>
